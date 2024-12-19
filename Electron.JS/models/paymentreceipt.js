ecae3e964faa5f812fb7be7992f88c54df91a46b
Ho Ngoc Trung Quan <hntqun0805@gmail.com>
2024-12-19 10:52:12 +0700

    Metx2

--- Electron.JS/models/paymentreceipt.js
+++ Electron.JS/models/paymentreceipt.js
@@ -1,43 +1,33 @@
-'use strict';
-const { Model } = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class PaymentReceipt extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      // this.belongsTo(models.Account, {
-      //   foreignKey: 'createdBy',
-      //   targetKey: 'personnelCode',
-      //   onDelete: 'SET NULL',
-      //   onUpdate: 'CASCADE',
-      // })
-      // this.belongsTo(models.Agency, {
-      //   foreignKey: 'agencyCode',
-      //   targetKey: 'agencyCode',
-      //   onDelete: 'SET NULL',
-      //   onUpdate: 'CASCADE',
-      // })
-    }
-  }
-  PaymentReceipt.init({
-    paymentReceiptCode: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      primaryKey: true,
-    },
-    paymentDate: DataTypes.DATE,
-    amount: DataTypes.DOUBLE,
-    agencyCode: DataTypes.STRING,
-    createdBy: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'PaymentReceipt',
-    freezeTableName: true,
-    tableName: 'PaymentReceipt',
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/database');
+
+const PaymentReceipt = sequelize.define('PaymentReceipt', {
+  paymentReceiptCode: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    primaryKey: true,
+  },
+  paymentDate: DataTypes.DATE,
+  amount: DataTypes.DOUBLE,
+  agentCode: DataTypes.STRING,
+  createdBy: DataTypes.STRING
+}, {
+  freezeTableName: true,
+});
+
+PaymentReceipt.associate = function(models) {
+  this.belongsTo(models.Account, {
+    foreignKey: 'createdBy',
+    targetKey: 'personnelCode',
+    onDelete: 'SET NULL',
+    onUpdate: 'CASCADE',
   });
-  return PaymentReceipt;
+  this.belongsTo(models.Agency, {
+    foreignKey: 'agentCode',
+    targetKey: 'agentCode',
+    onDelete: 'SET NULL',
+    onUpdate: 'CASCADE',
+  });
 };
+
+module.exports = { PaymentReceipt };
