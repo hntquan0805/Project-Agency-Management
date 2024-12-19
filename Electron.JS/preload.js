4968c98ff863877f33675d228644896a2688f807
minhthien278 <nguyenminhthien2708@gmail.com>
2024-12-19 10:54:21 +0700

    metX3

--- Electron.JS/preload.js
+++ Electron.JS/preload.js
@@ -2,10 +2,8 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('api', {
     signUp: (username, password) => ipcRenderer.invoke('sign-up', { username, password }),
-<<<<<<< HEAD
 
     addAgency: (agencyData) => ipcRenderer.invoke('add-agency', agencyData),
-=======
+    
     searchAgencies: (criteria) => ipcRenderer.invoke('search', criteria),
->>>>>>> 0fb44009e82b2aa9ba0a7e4d58e420c57469b3f3
 });
